f712635830da7b8718b033535801b487308db9ff
Avinash <avinash30122002@gmail.com>
2023-02-19 23:37:23 +0530

    Update

--- src/layout/Footer/Footer.js
+++ src/layout/Footer/Footer.js
@@ -5,7 +5,10 @@ const Footer = () => {
     <div className="bg-[#57318d] text-center py-3">
       <p className="text-[#fff] text-sm">
         © 2021 - All rights reserved. Designed by{" "}
-        <a href="https://www.google.com" className="text-[#6faaed]">
+        <a
+          href="https://estate-rent-demo.netlify.app/"
+          className="text-[#6faaed]"
+        >
           Estate
         </a>
       </p>
