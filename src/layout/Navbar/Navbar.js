45a4a2aaa85672ad6fe628a6b5d48be019e2192f
Avinash <avinash30122002@gmail.com>
2023-02-20 01:26:54 +0530

    Update

--- src/layout/Navbar/Navbar.js
+++ src/layout/Navbar/Navbar.js
@@ -7,8 +7,8 @@ import User from "./components/User";
 
 const Navbar = () => {
   return (
-    <div className="shadow-xl">
-      <div className="navbar w-[100%] bg-[#fcfaff]  flex flex-row justify-between items-center text-black py-3 px-10">
+    <div className="shadow-md shadow-[#e5cdff]">
+      <div className="navbar w-[100%] bg-[#fcfaff]  flex flex-row justify-between items-center text-black py-3 px-10 border-b-[1px] border-b-[#e5cdff]">
         <div className="flex flex-row h-10 items-center">
           <Logo />
           <Menu />
